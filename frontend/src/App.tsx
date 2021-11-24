059750adc92ef807530bdde3c5b1f802e379b98c
Alexander Ustyugov <ulexxander@gmail.com>
2021-11-24 21:52:15 +0100

    frontend - loading and displaying notes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,78 @@
+import { createEffect, createEvent, createStore, forward } from "effector";
+import { useStore } from "effector-react";
+import { useEffect } from "react";
+import { apiRequest, Note, NoteCreateParams } from "./api";
 import "./App.css";
 
+const pageMounted = createEvent();
+
+const $notes = createStore<Note[] | null>(null);
+const $notesError = createStore<Error | null>(null);
+
+const notesLoadFx = createEffect<void, Note[]>((params) =>
+  apiRequest("/notes/all", { method: "GET" })
+);
+
+const noteCreateFx = createEffect<NoteCreateParams, Note>((params) =>
+  apiRequest("/notes/create", {
+    method: "POST",
+    body: JSON.stringify(params),
+  })
+);
+
+$notes.on(notesLoadFx.doneData, (_, notes) => notes);
+$notesError.on(notesLoadFx.failData, (_, err) => err).reset(notesLoadFx);
+
+forward({
+  from: pageMounted,
+  to: notesLoadFx,
+});
+
+forward({
+  from: noteCreateFx.doneData,
+  to: notesLoadFx,
+});
+
+const NotesList: React.FC = () => {
+  const notes = useStore($notes);
+  const notesError = useStore($notesError);
+
+  if (notesError) {
+    return <p>Error: {notesError.message}</p>;
+  }
+  if (!notes) {
+    return <p>Loading...</p>;
+  }
+  if (!notes.length) {
+    return <p>No notes yet...</p>;
+  }
+
+  return (
+    <ul>
+      {notes.map((note) => (
+        <li key={note.ID}>
+          <p>{note.Text}</p>
+          <p>Created: {new Date(note.CreatedAt).toString()}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const Notes: React.FC = () => {
+  return (
+    <div>
+      <h2>My Notes</h2>
+      <NotesList />
+    </div>
+  );
+};
+
 export const App: React.FC = () => {
+  useEffect(() => {
+    pageMounted();
+  }, []);
+
   return (
     <div className="App">
       <header>
@@ -10,6 +82,8 @@ export const App: React.FC = () => {
         />
         <h1>Docker Workshop</h1>
       </header>
+
+      <Notes />
     </div>
   );
 };
