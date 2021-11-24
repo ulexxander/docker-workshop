89829dc1fb8202758153ad46920b8c4ff03ac62e
Alexander Ustyugov <ulexxander@gmail.com>
2021-11-24 20:14:53 +0100

    initialized frontend project (react + vite)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+export const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header>
+        <img
+          src="https://www.docker.com/sites/default/files/d8/styles/large/public/2021-08/Moby-share.png?itok=Kc8zKIm4"
+          alt="Docker is fun"
+        />
+        <h1>Docker Workshop</h1>
+      </header>
+    </div>
+  );
+};
