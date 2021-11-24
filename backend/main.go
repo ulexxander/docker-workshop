8ab3efe6ac392ac8572dafae843f1d204138624e
Alexander Ustyugov <ulexxander@gmail.com>
2021-11-24 20:48:55 +0100

    backend - hello world http endpoint in Go

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-	log.Println("hello")
+	log.Println("starting service")
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello\n")
+	})
+
+	addr, ok := os.LookupEnv("HTTP_SERVER_ADDR")
+	if !ok {
+		addr = ":80"
+	}
+
+	log.Println("listening http", addr)
+	err := http.ListenAndServe(addr, mux)
+	if err != nil {
+		log.Fatalf("error listening http: %s", err)
+	}
 }
